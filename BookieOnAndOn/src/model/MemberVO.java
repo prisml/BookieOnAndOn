54c2f42b499032f0bcb4c7137f69487ec021a278
8story8 <8story8@naver.com>
2017-04-05 17:47:22 +0900

    temp 4

--- BookieOnAndOn/src/model/MemberVO.java
+++ BookieOnAndOn/src/model/MemberVO.java
@@ -1,5 +1,49 @@
 package model;
 
-public class MemberVO {
+public class MemberVO implements VO{
+	private String id;
+	private String password;
+	private String name;
+	private String tel;
+	public MemberVO() {
+		super();
+	}
+	public MemberVO(String id, String password, String name, String tel) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.tel = tel;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [id=" + id + ", password=" + password + ", name=" + name + ", tel=" + tel + "]";
+	}
+	
+	
 
 }
