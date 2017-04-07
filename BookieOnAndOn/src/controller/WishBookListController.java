233f2394e45a2303629409f751470cb1017ccf17
KOSTA <KOSTA@KOSTA-HP>
2017-04-07 11:39:34 +0900

    mypage 수정

--- BookieOnAndOn/src/controller/WishBookListController.java
+++ BookieOnAndOn/src/controller/WishBookListController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import model.ListVO;
+import model.MemberVO;
 import model.PagingBean;
 import model.SawWishDAO;
 import model.VO;
@@ -14,24 +16,33 @@ public class WishBookListController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	
 		response.setContentType("text/html;charset=utf-8");
-		
-			String java="java";
-		int totalContent=SawWishDAO.getInstance().getWishTotalContent(java);
-			String nowpage=request.getParameter("nowPage");
-			if(nowpage==null){
-				nowpage="1";
+		String url = "";
+
+		HttpSession session = request.getSession();
+		MemberVO vo = (MemberVO) session.getAttribute("mvo");
+		if (vo == null) {
+
+			url = "index.jsp";
+
+		} else {
+			int totalContent = SawWishDAO.getInstance().getWishTotalContent(vo.getId());
+			String nowpage = request.getParameter("nowPage");
+			if (nowpage == null) {
+				nowpage = "1";
 			}
-		int nowPage=Integer.parseInt(nowpage);
-		PagingBean pagingBean=new PagingBean(totalContent,nowPage);
-		
-		ArrayList<VO> list=SawWishDAO.getInstance().getWishBookList(java, pagingBean);
-		System.out.println("list~~~~~~~~~~~"+list);
-		ListVO listvo=new ListVO(list,pagingBean);
-		System.out.println(listvo.getList());
-		request.setAttribute("listVO", listvo);
-		
-		return "bookieOnAndOn/wishBookList.jsp";
+			int nowPage = Integer.parseInt(nowpage);
+			PagingBean pagingBean = new PagingBean(totalContent, nowPage);
+
+			ArrayList<VO> list = SawWishDAO.getInstance().getWishBookList(vo.getId(), pagingBean);
+			ListVO listvo = new ListVO(list, pagingBean);
+			session.setAttribute("listVO", listvo);
+			url = "redirect:bookieOnAndOn/wishBookList.jsp";
+
+		}
+
+		return url;
 	}
 
 }
