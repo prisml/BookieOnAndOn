0000036bf9a4943622ee6f3a2d590c35c7d26bec
8story8 <8story8@naver.com>
2017-04-07 16:52:03 +0900

    도서 자동 검색 기능 완료!

---
+++ BookieOnAndOn/src/controller/BookAutoCompleteController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import model.BookDAO;
+import model.ListVO;
+import model.PagingBean;
+import model.VO;
+
+public class BookAutoCompleteController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		response.setContentType("text/html; charset=utf-8");
+		String title = request.getParameter("title");
+		PrintWriter out = response.getWriter();
+		ArrayList<VO> bookAutoCompleteList = BookDAO.getInstance().getBookAutoCompleteList(title);
+		JSONArray jsa = new JSONArray(bookAutoCompleteList);
+		out.print(jsa);
+		return "AjaxView";
+	}
+}
